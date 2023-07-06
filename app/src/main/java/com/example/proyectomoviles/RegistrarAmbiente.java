e5799d0227ce8d131d5076ecacef8fb56c16c530
Jose L Rivas <joseluisro1595@gmail.com>
2023-07-05 23:48:27 -0500

    Agrego actividad Registrar ambiente

--- app/src/main/java/com/example/proyectomoviles/RegistrarAmbiente.java
+++ app/src/main/java/com/example/proyectomoviles/RegistrarAmbiente.java
@@ -6,18 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class ListaAmbientes extends AppCompatActivity {
+public class RegistrarAmbiente extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lista_ambientes);
+        setContentView(R.layout.activity_registrar_ambiente);
     }
 
-    //Mètodo boton Agregar
-    public void Agregar(View view)
+    public void GuardarAmbiente(View view)
     {
-        Intent siguiente = new Intent(this,RegistrarAmbiente.class);
+        Intent siguiente = new Intent(this,ListaAmbientes.class);
         startActivity(siguiente);
 
     }

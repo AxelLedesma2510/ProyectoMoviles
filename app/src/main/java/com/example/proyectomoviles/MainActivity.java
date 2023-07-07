1654399e19cddf0725945259ecee3cdbb4052040
Axel Lucas Ledesma Vargas <134738464+AxelLedesma2510@users.noreply.github.com>
2023-07-06 23:34:07 -0500

    Fix font - internet permission - Add Volley dependency

--- app/src/main/java/com/example/proyectomoviles/MainActivity.java
+++ app/src/main/java/com/example/proyectomoviles/MainActivity.java
@@ -1,10 +1,22 @@
 package com.example.proyectomoviles;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +45,29 @@ public class MainActivity extends AppCompatActivity {
     {
         Intent siguiente = new Intent(this,Acercade.class);
         startActivity(siguiente);
+    }
+
+    public void Conectar(View view)
+    {
+        ejecutarServicio("https://webscapy.000webhostapp.com/database.php");
+    }
+
+    private void ejecutarServicio(String URL)
+    {
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getApplicationContext(), "OPERACION EXITOSA", Toast.LENGTH_SHORT).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),error.toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+         RequestQueue requestQueue= Volley.newRequestQueue(this);
+         requestQueue.add(stringRequest);
     }
 
 }
--- app/src/main/java/com/example/proyectomoviles/RegistrarAmbiente.java
+++ app/src/main/java/com/example/proyectomoviles/RegistrarAmbiente.java
@@ -5,17 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class RegistrarAmbiente extends AppCompatActivity {
+
+    EditText eTNomAmbiente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrar_ambiente);
+        eTNomAmbiente=(EditText)findViewById(R.id.eTNomAmbiente);
+
     }
 
     public void GuardarAmbiente(View view)
     {
+
         Intent siguiente = new Intent(this,ListaAmbientes.class);
         startActivity(siguiente);
 

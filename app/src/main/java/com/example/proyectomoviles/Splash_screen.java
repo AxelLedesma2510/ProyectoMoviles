b9271f4cb4aa307dde84562885d795192f9f3dc7
GianeSP2001 <gianesandoval@hotmail.com>
2023-07-07 18:11:53 -0500

    Avance 1.0 splash

---
+++ app/src/main/java/com/example/proyectomoviles/Splash_screen.java
@@ -0,0 +1,24 @@
+package com.example.proyectomoviles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash_screen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
+    }
+}

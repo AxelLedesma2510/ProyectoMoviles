01f2f0a305e5d6e6b328e9eec4fc1fa1d045a858
GianeSP2001 <gianesandoval@hotmail.com>
2023-07-07 19:39:03 -0500

    Avance 2.0 splash

--- app/src/main/java/com/example/proyectomoviles/SplashActivity.java
+++ app/src/main/java/com/example/proyectomoviles/SplashActivity.java
@@ -5,20 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.WindowManager;
 
-public class Splash_screen extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(Splash_screen.this, MainActivity.class);
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intent);
                 finish();
             }
-        }, 3000);
+        }, 4000);
     }
 }
